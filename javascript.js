31d70cb879d841e0418300a231ffa5d12540eadf
Jaryd Yia <jjaryd@Gmail.com>
2022-05-19 22:12:22 -0400

    Make a random number generate from 1 to 3

---
+++ javascript.js
@@ -0,0 +1,15 @@
+console.log("MEOWWWW")
+console.log(Math.floor(Math.random(4) * 3) + 1);
+
+function numberGenerator(){
+    // gives you a random number from 1 - 3
+    let randomNumber = (Math.floor(Math.random(4) * 3) + 1)
+    return randomNumber;
+}
+
+function computerPlay(){
+let rock = 1;
+let paper = 2;
+let scissors = 3;
+
+}
