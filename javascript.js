169ef0652608433edf462e4417c88b70b53b6c4a
Jaryd Yia <jjaryd@Gmail.com>
2022-05-19 22:23:45 -0400

    Create computerPlay function to return rock paper or scissors randomly

--- javascript.js
+++ javascript.js
@@ -1,15 +1,19 @@
 console.log("MEOWWWW")
 console.log(Math.floor(Math.random(4) * 3) + 1);
 
-function numberGenerator(){
-    // gives you a random number from 1 - 3
-    let randomNumber = (Math.floor(Math.random(4) * 3) + 1)
-    return randomNumber;
-}
-
 function computerPlay(){
-let rock = 1;
-let paper = 2;
-let scissors = 3;
+      // gives you a random number from 1 - 3
+      let randomNumber = (Math.floor(Math.random(4) * 3) + 1);
+
+    if (randomNumber === 1){
+        console.log("Rock");
+        return "Rock";
+    } else if ( randomNumber === 2 ){
+        console.log("Paper");
+        return "Paper";
+    } else if (randomNumber === 3){
+        console.log("Scissors");
+        return "Scissors";
+    }
 
 }
